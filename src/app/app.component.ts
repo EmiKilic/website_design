b8250274ce0ca6b9877aad8511cd01a51d355a9e
EmiKilic <emirhan_kilic@outlook.de>
2025-01-03 20:16:24 +0100

    added stacking cards component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import {MatButtonModule} from '@angular/material/button';
 import { FirstPartComponent } from './first-part/first-part.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProcessComponent } from './process/process.component';
+import { StackingCardsComponent } from './stacking-cards/stacking-cards.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, ProcessComponent,MatButtonModule, FirstPartComponent, CarouselComponent],
+  imports: [RouterOutlet, MatToolbarModule, ProcessComponent,MatButtonModule, StackingCardsComponent,FirstPartComponent, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/stacking-cards/stacking-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stacking-cards',
+  imports: [],
+  templateUrl: './stacking-cards.component.html',
+  styleUrl: './stacking-cards.component.scss'
+})
+export class StackingCardsComponent {
+
+}
