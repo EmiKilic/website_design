14d198fa4a8bf4f9e1e93310bdc7e4cb18cb168f
EmiKilic <emirhan_kilic@outlook.de>
2025-01-05 16:56:18 +0100

    added faq area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button'; 
 import { FirstPartComponent } from './first-part/first-part.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ProcessComponent } from './process/process.component';
 import { StackingCardsComponent } from './stacking-cards/stacking-cards.component';
+import { FAQSComponent } from './faqs/faqs.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, ProcessComponent,MatButtonModule, StackingCardsComponent,FirstPartComponent, CarouselComponent],
+  imports: [MatToolbarModule, ProcessComponent,MatButtonModule, StackingCardsComponent,FirstPartComponent, CarouselComponent, FAQSComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/faqs/faqs.component.ts
@@ -0,0 +1,15 @@
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-faqs',
+  imports: [MatCard, MatIcon, MatButtonModule, MatExpansionModule],
+  templateUrl: './faqs.component.html',
+  styleUrl: './faqs.component.scss',
+})
+export class FAQSComponent {
+    readonly panelOpenState = signal(false);
+}
