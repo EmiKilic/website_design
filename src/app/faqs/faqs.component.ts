0bc0a2356116f206d716b224765d6376a8b0e9d4
EmiKilic <emirhan_kilic@outlook.de>
2025-01-07 15:12:33 +0100

    responsiveness on faw added

--- src/app/faqs/faqs.component.ts
+++ src/app/faqs/faqs.component.ts
@@ -2,11 +2,11 @@ import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-faqs',
-  imports: [MatCard, MatIcon, MatButtonModule, MatExpansionModule],
+  imports: [MatCard, MatIcon, MatButtonModule, MatExpansionModule, MatAccordion],
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.scss',
 })
